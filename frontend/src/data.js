0670f9517876097520d81e7247bcdfaf28f11c5c
dannyta556 <dannyta556@gmail.com>
2023-01-05 15:22:12 -0800

    add some sample products

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+  products: [
+    {
+      name: 'Ash Blossom & Joyous Spring',
+      slug: 'ash-blossom-&-joyous-spring',
+      category: 'single',
+      image: '/images/p1.jpg',
+      price: 4,
+      countInStock: 10,
+      brand: 'Yugioh',
+      rating: 4.5,
+      numReviews: 10,
+      number: 'MACR-EN036',
+      rarity: 'Secret Rare',
+      level: 3,
+      description: `During either player's turn, when a card or effect is activated that includes any of these effects: You can discard this card; negate that effect.
+- Add a card from the Deck to the hand.
+- Special Summon from the Deck.
+- Send a card from the Deck to the Graveyard.
+You can only use this effect of "Ash Blossom & Joyous Spring" once per turn.`,
+    },
+    {
+      name: 'Tearlaments Scheiren',
+      slug: 'tearlaments-scheiren',
+      category: 'single',
+      image: '/images/p2.jpg',
+      price: 1.69,
+      countInStock: 10,
+      brand: 'Yugioh',
+      rating: 4.5,
+      numReviews: 10,
+      number: 'POTE-EN014',
+      rarity: 'Super Rare',
+      level: 4,
+      description: `During your Main Phase: You can Special Summon this card from your hand, and if you do, send 1 monster from your hand to the GY, then, send the top 3 cards of your Deck to the GY. If this card is sent to the GY by card effect (except during the Damage Step): You can Fusion Summon 1 Fusion Monster from your Extra Deck, by placing Fusion Materials mentioned on it from your hand, field, and/or GY, including this card from your GY, on the bottom of the Deck in any order. You can only use each effect of "Tearlaments Scheiren" once per turn.`,
+    },
+    {
+      name: 'Despian Quaertis',
+      slug: 'despian-quaertis',
+      category: 'single',
+      image: '/images/p3.jpg',
+      price: 4,
+      countInStock: 10,
+      brand: 'Yugioh',
+      rating: 4.5,
+      numReviews: 10,
+      number: 'MP22-EN141',
+      rarity: 'Prismatic Secret Raree',
+      level: 8,
+      description: `1 "Despia" monster + 1 LIGHT or DARK monster
+During the Main Phase (Quick Effect): You can change the ATK of all monsters currently on the field to 0 until the end of this turn, except Level 8 or higher Fusion Monsters. If this face-up card in its owner's control leaves the field because of an opponent's card effect: You can add to your hand, or Special Summon, 1 "Fallen of Albaz" or 1 "Despia" monster, from your Deck. You can only use each effect of "Despian Quaeritis" once per turn.`,
+    },
+    {
+      name: 'The Dark Magicians',
+      slug: 'the-dark-magicians',
+      category: 'single',
+      image: '/images/p4.jpg',
+      price: 8.15,
+      countInStock: 10,
+      brand: 'Yugioh',
+      rating: 4.0,
+      numReviews: 10,
+      number: 'LDS3-EN090',
+      rarity: 'Secret Rare',
+      level: 8,
+      description: `"Dark Magician" or "Dark Magician Girl" + 1 Spellcaster monster Once per turn, if a Spell/Trap Card or effect is activated (except during the Damage Step): You can draw 1 card, then if it was a Spell/Trap, you can Set it, and if it was a Trap or Quick-Play Spell, you can activate it this turn. If this card is destroyed: You can Special Summon both 1 "Dark Magician" and 1 "Dark Magician Girl" from your hand, Deck, and/or GY.`,
+    },
+  ],
+};
+
+export default data;
